570a861963d2b7e15481c1ffb11315282fb9e6c0
glbreeze <glbreeze@gmail.com>
2022-05-23 15:21:32 +0800

    fix

--- model/ops/make.sh
+++ model/ops/make.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # ------------------------------------------------------------------------------------------------
 # Deformable DETR
 # Copyright (c) 2020 SenseTime. All Rights Reserved.
@@ -7,4 +7,28 @@
 # Modified from https://github.com/chengdazhi/Deformable-Convolution-V2-PyTorch/tree/pytorch_1.0.0
 # ------------------------------------------------------------------------------------------------
 
-python setup.py build install
+
+#SBATCH --job-name=seg
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=lg154@nyu.edu
+#SBATCH --output=seg.out
+#SBATCH --gres=gpu # How much gpu need, n is the number
+
+module purge
+
+
+echo "start"
+singularity exec --nv \
+            --overlay /scratch/lg154/python36/python36.ext3:rw \
+            /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif \
+            /bin/bash -c "source /ext3/env.sh; python setup.py build install > log.txt 2>&1"
+
+echo "finish"
+
+
+#GREENE GREENE_GPU_MPS=yes
